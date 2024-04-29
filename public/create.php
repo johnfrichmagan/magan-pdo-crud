bb6017bd4cf51ca1d4a4a6a501d0ab90bf38e449
magan <johnfrichmagan530@gmail.com>
2024-04-29 19:05:07 +0800

    updated logout.php

--- public/create.php
+++ public/create.php
@@ -55,7 +55,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             // Attempt to execute the prepared statement
             if($stmt->execute()){
                 // Records created successfully. Redirect to landing page
-                header("location: ../index.php");
+                header("location: ../public/welcome.php");
                 exit();
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
