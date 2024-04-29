c173bb9e6eaddf0f08afa956ad4c4f3bb5fef176
johnmagan <johnfrichmagan530@gmail.com>
2024-04-29 17:17:43 +0800

    added index.php

--- db/config.php
+++ db/config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'u593341949_dev_magan');
-define('DB_PASSWORD', '20221492Magan');
-define('DB_NAME', 'u593341949_db_magan');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'magan');
  
 /* Attempt to connect to MySQL database */
 try{
